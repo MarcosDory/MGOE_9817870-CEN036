905f6e32b28556ec720a5e60113fba2674811ebf
MarcosDory <marcosalexandre1940@hotmail.com>
2021-09-30 14:21:54 +0000

    Adicionando a questão 2

---
+++ bash_script.sh
@@ -0,0 +1,6 @@
+#Comando que mostre a pasta ou diretório onde estou:
+pwd
+#Comando que gere uma lista com detalhes do conteúdo da HOME:
+ls -l
+#Comando que imprima a data atual na tela:
+print(30/09/2021)
